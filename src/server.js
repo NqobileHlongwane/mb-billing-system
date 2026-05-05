758f84777422214bdffd017cd0e84cd512eca192
NqobileHlongwane <nqobiletototo@gmail.com>
2026-05-06 00:02:30 +0200

    added urls to app.jsx and server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://fantastic-cupcake-c57282.netlify.app/",
   credentials: true
 }));
 app.use(express.json())
